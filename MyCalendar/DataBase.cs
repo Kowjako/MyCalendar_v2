328969e3714bce5cfa6be4b82618bfcad4bc8e56
Włodzimierz Kowjako <kowyako@yandex.ru>
2020-07-18 12:13:50 +0200

    Add database class
    database MySQL class

---
+++ MyCalendar/DataBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+namespace MyCalendar
+{
+    class DataBase
+    {
+        MySqlConnection connection = new MySqlConnection("server=127.0.0.1;port=3306;username=root;password=;database=eventdb");
+        public void startConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+                connection.Open();
+        }
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+                connection.Close();
+        }
+        public MySqlConnection getConnection()
+        {
+            return connection;
+        }
+    }
+}
